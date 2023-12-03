d44db2f879792329c97f641c697cb93c4cf174a7
Soorya U <sooryau7@gmail.com>
2023-12-03 18:02:08 +0530

    Connected to Mongoose

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const urlRoute = require("./routers/url");
-const connectMongo = require("./connect");
+const connectMongo = require("./connection");
 
 const app = express();
 const PORT = 7000;
 
-connectMongo("mongodb://localhost:27017/url-shortner").then(() =>
+connectMongo("mongodb://127.0.0.1:27017/url-shortner").then(() =>
   console.log(`MongoDB Connected`)
 );
 
